fd0b5cddecc2fe6abc866a208cb5414e76f0fd6e
Alexsandre Almeida <aralmeida@mpes.mp.br>
2020-02-06 19:00:35 -0300

    change test for real case and implement in GCP

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexsandre/Ding
+
+go 1.12
+
+require github.com/PuerkitoBio/goquery v1.5.1
--- main.go
+++ main.go
@@ -1,17 +1,53 @@
-package main
+package ding
 
 import (
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func main() {
-	lambda.Start(handler)
+func Clipping(w http.ResponseWriter, r *http.Request) {
+	doc, err := requestHtmlSearch()
+	if err != nil {
+		return
+	}
+
+	if (*doc).Find("tr.odd").Length() > 0 {
+		fmt.Println("Deu certo")
+	} else {
+		fmt.Println("Não deu certo")
+	}
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
-	}, nil
+func requestHtmlSearch() (*goquery.Document, error) {
+	urlDimpes := "http://dimpes.mpes.mp.br/ResultadoPesquisa"
+	termoBusca := os.Getenv("termoBusca")
+
+	formData := url.Values{
+		"palavra": {termoBusca},
+		"dataDe":  {time.Now().Local().Format("02-01-2006")},
+		"dataAte": {time.Now().Local().Format("02-01-2006")},
+	}
+
+	resp, err := http.PostForm(urlDimpes, formData)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, errors.New("StatusCode != 200")
+	}
+
+	htmlDoc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return htmlDoc, nil
 }
